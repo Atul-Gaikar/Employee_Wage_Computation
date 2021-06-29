669f5c6785c0c77a04266b8809dea402146bbc38
Atul <atulgaikarag9@gmail.com>
2021-06-29 13:55:05 +0530

    Employee Wage Computation

---
+++ src/com/bridgelabz/employeewagecomputation/EmployeePresentAbsentUC1.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.employeewagecomputation;
+
+public class EmployeePresentAbsentUC1 {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to employee wage");
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		// checking the presence of Employee
+		if (empCheck == IS_FULL_TIME) {
+			System.out.println(" Employee is Present ");
+		} else
+			System.out.println(" Employee is Absent ");
+	}
+}
